3eefde30807f94c2171754a05748dfc96122b908
mcdanije22 <mcdanije22@gmail.com>
2019-11-16 00:02:43 -0500

    gallery page

---
+++ src/pages/gallery.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./styles/gallery.scss"
+
+import SEO from "../components/seo"
+import Layout from "../components/layout"
+
+const GalleryPage = () => (
+  <Layout>
+    <SEO title="Gallery" />
+    <div>
+      <h1>Gallery</h1>
+      <div id="galleryContainer">
+        <img src="https://via.placeholder.com/150" />
+        <img src="https://via.placeholder.com/150" />
+        <img src="https://via.placeholder.com/150" />
+        <img src="https://via.placeholder.com/150" />
+        <img src="https://via.placeholder.com/150" />
+        <img src="https://via.placeholder.com/150" />
+        <img src="https://via.placeholder.com/150" />
+        <img src="https://via.placeholder.com/150" />
+      </div>
+    </div>
+  </Layout>
+)
+
+export default GalleryPage
